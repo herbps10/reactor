95c9bb4b554efac841238e01f4dd140daa653a05
Herb Susmann <herbps10@gmail.com>
2019-07-30 22:20:51 -0400

    Updated frontend

--- inst/frontend/service-worker.js
+++ inst/frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74b6b632a2cf90515d1591a9aac38fd2.js"
+  "/precache-manifest.7cdee8eacb73983bf1643d397e05d5bc.js"
 );
 
 self.addEventListener('message', (event) => {
